ffec183925954d0414968a2d4226eb0b65d82934
alexandreweb1 <alexandreweb2@gmail.com>
2026-01-19 16:38:44 +0000

    Prettier ignore

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Quer sair comigo? Se sim, da um sorriso para mim!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("Teste2");
-}
-
 export default Home;
