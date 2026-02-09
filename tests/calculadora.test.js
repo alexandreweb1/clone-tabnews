7b8da0cdf1fd84e7eff40d0a94cd6974d9b7606b
alexandreweb1 <alexandreweb2@gmail.com>
2026-02-09 09:58:58 +0000

    Adjust in test

---
+++ tests/calculadora.test.js
@@ -0,0 +1,10 @@
+test("nome do teste", () =>{
+  console.log("e agora?");
+});
+
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  console.log(resultado);
+})
