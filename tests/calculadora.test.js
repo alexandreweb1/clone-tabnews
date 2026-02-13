379de90b41ecd64355077e31f11855102a8e1fc2
alexandreweb1 <alexandreweb2@gmail.com>
2026-02-13 15:48:33 +0000

    Install jest for tests

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -6,5 +6,5 @@ const calculadora = require("../models/calculadora.js");
 
 test("somar 2 + 2 deveria retornar 4", () => {
   const resultado = calculadora.somar(2, 2);
-  console.log(resultado);
+  expect(resultado).toBe(4);d
 })
